af4c9361d8a320c5fa060388e949453903f5a970
salim928 <salimadams49@gmail.com>
2023-01-19 01:44:31 +0000

    inital commit

---
+++ lookup/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Home(request):
+    return render(request, 'Home.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
